f65d7ab1e4212e9562c1506cad317c6e80ac5679
yan2700 <tomoyahyuga1213@gmail.com>
2025-04-25 17:31:25 +0900

    main

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,23 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
  
-  def after_sign_in_path_for(resource)
-    items_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   items_path
+  # end
 
   
-  def after_sign_up_path_for(resource)
-    items_path
-  end
+  # def after_sign_up_path_for(resource)
+  #   items_path
+  # end
 
-  private
+  # private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :nickname, :birthday, :last_name, :first_name, :last_name_kana, :first_name_kana
-    ])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [
+  #     :nickname, :birthday, :last_name, :first_name, :last_name_kana, :first_name_kana
+  #   ])
+  # end
 end
--- db/migrate/20250425012642_remove_birthday_from_users.rb
+++ db/migrate/20250425012642_remove_birthday_from_users.rb
@@ -1,5 +1,5 @@
-class RemoveBirthdayFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :birthday, :date
-  end
-end
+# class RemoveBirthdayFromUsers < ActiveRecord::Migration[7.1]
+#   def change
+#     remove_column :users, :birthday, :date
+#   end
+# end
