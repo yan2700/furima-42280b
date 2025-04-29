eb8971384b11e60ed4424e1ead8946995145802c
yan2700 <tomoyahyuga1213@gmail.com>
2025-04-29 13:59:47 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,6 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
   belongs_to :user
-  has_one :order
   has_one_attached :image
 
   with_options presence: true do
