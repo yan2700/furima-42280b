9b448bc35494f0289cd6c8f7d46b78ef972d7b30
yan2700 <tomoyahyuga1213@gmail.com>
2025-04-29 14:09:53 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
@@ -13,37 +14,36 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品が出品できない場合' do
-      it 'カテゴリーが空だと登録できない' do
-        @item.category_id = nil
+      it 'カテゴリーが未選択だと登録できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors[:category_id]).to include("can't be blank")
       end
 
-      it 'ステータスが空だと登録できない' do
-        @item.status_id = nil
+      it 'ステータスが未選択だと登録できない' do
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors[:status_id]).to include("can't be blank")
       end
 
-      it '配送料負担が空だと登録できない' do
-        @item.shipping_fee_status_id = nil
+      it '配送料負担が未選択だと登録できない' do
+        @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors[:shipping_fee_status_id]).to include("can't be blank")
       end
 
-      it '発送元の地域が空だと登録できない' do
-        @item.prefecture_id = nil
+      it '発送元の地域が未選択だと登録できない' do
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors[:prefecture_id]).to include("can't be blank")
       end
 
-      it '発送までの日数が空だと登録できない' do
-        @item.scheduled_delivery_id = nil
+      it '発送までの日数が未選択だと登録できない' do
+        @item.scheduled_delivery_id = 1
         @item.valid?
         expect(@item.errors[:scheduled_delivery_id]).to include("can't be blank")
       end
 
-      
       it '商品名が空だと登録できない' do
         @item.name = ''
         @item.valid?
@@ -78,20 +78,18 @@ RSpec.describe Item, type: :model do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors[:price]).to include("must be less than or equal to 9999999")
+      end
 
-        
       it '価格が半角数値以外では登録できない' do
-       @item.price = '１０００'  
-       @item.valid?
-       expect(@item.errors[:price]).to include('is not a number')
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors[:price]).to include('is not a number')
       end
 
       it 'userが紐づいていないと登録できない' do
         @item.user = nil
         @item.valid?
-       expect(@item.errors[:user]).to include("must exist")
-     end
-
+        expect(@item.errors[:user]).to include("must exist")
       end
     end
   end
